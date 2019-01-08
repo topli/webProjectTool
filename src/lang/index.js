b1eff57f051bf2e60e669f75557b3a40130dc471
topli <199720923@qq.com>
2019-01-08 16:23:14 +0800

    2

--- src/lang/index.js
+++ src/lang/index.js
@@ -4,8 +4,15 @@ import Cookies from 'js-cookie';
 import elementEnLocale from 'element-ui/lib/locale/lang/en'; // element-ui lang
 import elementZhLocale from 'element-ui/lib/locale/lang/zh-CN';// element-ui lang
 import enLocale from './en';
-import zhLocale from './zh';
+// import zhLocale from './zh';
 
+// loading zh lang
+const requireAll = requireContext => requireContext.keys().map(requireContext);
+const req = requireAll(require.context('./zh', false, /\.js$/));
+let zhLocale = {};
+req.map(r => {
+  zhLocale = Object.assign(zhLocale, r.default);
+});
 Vue.use(VueI18n);
 
 const messages = {
